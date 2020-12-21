ae0af092512c7ad41ec2f641c26825f8d4e6d7d5
lzyprime <2383518170@qq.com>
2020-12-21 11:41:13 +0800

    解决 在item2页面点击返回键会返回item1, 而非退出 问题

--- app/src/main/java/io/lzyprime/bottomnavigation/Item3Fragment.kt
+++ app/src/main/java/io/lzyprime/bottomnavigation/Item3Fragment.kt
@@ -1,5 +1,16 @@
 package io.lzyprime.bottomnavigation
 
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 
-class Item3Fragment:Fragment(R.layout.item3_fragment)
+class Item3Fragment:Fragment(R.layout.item3_fragment){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        })
+    }
+}
