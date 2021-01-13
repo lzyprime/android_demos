06500e33fd0b5ff151039e354a35a670a0d9211d
lzyprime <2383518170@qq.com>
2021-01-13 16:09:05 +0800

    kotlin-android-extensionæs 插件退役。  ViewBinding启用

--- app/src/main/java/io/lzyprime/mvvmdemo/MainActivity.kt
+++ app/src/main/java/io/lzyprime/mvvmdemo/MainActivity.kt
@@ -2,8 +2,8 @@ package io.lzyprime.mvvmdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -11,8 +11,8 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
+import io.lzyprime.mvvmdemo.databinding.ActivityMainBinding
 import io.lzyprime.mvvmdemo.viewmodels.ListPhotoViewModel
-import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,17 +24,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
-        refreshBtn.setOnClickListener {
+        binding.refreshBtn.setOnClickListener {
             model.refreshListPhotos()
         }
 
-        photoList.layoutManager = GridLayoutManager(this, 2)
+        binding.photoList.layoutManager = GridLayoutManager(this, 2)
         model.listPhotos.observe(this) { photos ->
-            photoList.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+            binding.photoList.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                 override fun onCreateViewHolder(
                     parent: ViewGroup,
                     viewType: Int
