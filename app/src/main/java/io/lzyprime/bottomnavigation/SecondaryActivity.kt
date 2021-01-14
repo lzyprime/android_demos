06738f59705b7e2849f9165cd2bb5191581ee854
lzyprime <2383518170@qq.com>
2021-01-14 15:51:31 +0800

    弃用kotlin-android-extensions，启用viewBinding

--- app/src/main/java/io/lzyprime/bottomnavigation/SecondaryActivity.kt
+++ app/src/main/java/io/lzyprime/bottomnavigation/SecondaryActivity.kt
@@ -2,37 +2,39 @@ package io.lzyprime.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import kotlinx.android.synthetic.main.activity_secondary.*
+import io.lzyprime.bottomnavigation.databinding.ActivitySecondaryBinding
 
 class SecondaryActivity : AppCompatActivity() {
 
     private val fragments = arrayOf(Item1Fragment(), Item2Fragment(), Item3Fragment())
     private val itemIds = arrayOf(R.id.item1, R.id.item2, R.id.item3)
-
+    private lateinit var binding: ActivitySecondaryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_secondary)
+        binding = ActivitySecondaryBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
-        secondaryVP.adapter = object : FragmentStateAdapter(supportFragmentManager, lifecycle) {
+        binding.secondaryVP.adapter = object : FragmentStateAdapter(supportFragmentManager, lifecycle) {
             override fun getItemCount(): Int = fragments.size
             override fun createFragment(position: Int): Fragment = fragments[position]
         }
         // 页面预加载
-        secondaryVP.offscreenPageLimit = fragments.size
+        binding.secondaryVP.offscreenPageLimit = fragments.size
 
         // 若不想滑动切换页面时设置
         //secondaryVP.isUserInputEnabled = false
-        secondaryVP.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+        binding.secondaryVP.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
-                secondaryBtmNavView.selectedItemId = itemIds[position]
+                binding.secondaryBtmNavView.selectedItemId = itemIds[position]
             }
         })
 
-        secondaryBtmNavView.setOnNavigationItemSelectedListener {
-            secondaryVP.currentItem = itemIds.indexOf(it.itemId)
+        binding.secondaryBtmNavView.setOnNavigationItemSelectedListener {
+            binding.secondaryVP.currentItem = itemIds.indexOf(it.itemId)
             true
         }
     }
